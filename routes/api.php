5d6887190c843dd2c858e23a07227e279ea4ec03
fahlanlogic <alanfahdi@gmail.com>
2026-07-09 15:41:03 +0700

    feat: initialize product module

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
   return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/products', function () {
+  return response()->json([
+    'products' => [
+      [
+        'id' => 1,
+        'name' => 'Product 1',
+        'price' => 10.99,
+      ],
+      [
+        'id' => 2,
+        'name' => 'Product 2',
+        'price' => 19.99,
+      ],
+      [
+        'id' => 3,
+        'name' => 'Product 3',
+        'price' => 5.99,
+      ],
+    ]
+  ]);
+});
