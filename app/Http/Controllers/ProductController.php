af00d576784bb295fb450008e4fd5a2092b17167
fahlanlogic <alanfahdi@gmail.com>
2026-07-10 11:45:23 +0700

    tes

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,9 +18,9 @@ class ProductController extends Controller
                         'price' => 10.99,
                     ],
                     [
-                        'id' => 1,
-                        'name' => 'Product 1',
-                        'price' => 10.99,
+                        'id' => 2,
+                        'name' => 'Product 2',
+                        'price' => 20.99,
                     ],
                 ],
             ],
